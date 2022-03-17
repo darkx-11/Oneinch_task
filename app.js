9b7e86533483b8c77b65bb09d234ec8065775a5a
darkx-11 <crazyaditya11@gmail.com>
2022-03-17 22:50:25 +0530

    Try req query

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+//const router = express.Router();
+//import * as Controller from "./controller/oneInch.js";
+
+const oneinchRouter = require("./controller/router");
+app.use("/", oneinchRouter);
+
+//router.get("/", Controller.test);
+
+app
+  .listen(9000, () => {
+    console.log("Server Started");
+  })
+  .on("error", function (err) {
+    console.log("Error!", err);
+  });
